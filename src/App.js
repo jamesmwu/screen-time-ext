50fdad7f914c2112b510fbfee37cab71f890bd6d
jameswu <jamesw0462@gmail.com>
2022-11-24 13:08:49 -0800

    Used chrome tabs to identify when user is on hardcoded site, beginning to integrate stopwatch and timer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+/*global chrome*/
+
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import StopWatch from './components/Stopwatch';
+import Timer from './components/Timer';
+
+const sites = [
+  {
+    name: 'Youtube',
+    url: 'https://www.youtube.com/',
+  },
+];
 
 function App() {
+  const [items, setItems] = useState(sites);
+  const [time, setTime] = useState('none');
+
+  useEffect(() => {
+    const queryInfo = { active: true, lastFocusedWindow: true };
+
+    chrome.tabs &&
+      chrome.tabs.query(queryInfo, (tabs) => {
+        const url = tabs[0].url;
+        if (url.includes('youtube.com')) {
+          setTime('on youtube');
+        }
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='content'>
+      <h1>Choose website</h1>
+      {items.map((item) => {
+        return (
+          <div className='entry' key={item.name}>
+            <p style={{ marginRight: '0.5rem' }}>{item.name}:</p>
+            <p>{time}</p>
+            {/* <StopWatch /> */}
+          </div>
+        );
+      })}
+      <button className='btn'>Add</button>
     </div>
   );
 }
